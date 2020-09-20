b97687a7b9ddcb78c111664f05f51184095a38df
Настя Каспрук <anastasiyakaspruk17@gmail.com>
2020-09-20 18:52:29 +0300

    task5

---
+++ Lesson1/Lesson1/Tasks/Task5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+
+namespace Lesson1.Tasks
+{
+    public class Task5
+    {
+        /// <summary>
+        /// Generate IObservable from Task
+        /// (original: Generate Mono from Callable)
+        /// </summary>
+        public static IObservable<string> CreateSequence(Task<string> callable) =>
+            callable.ToObservable();
+    }
+}
